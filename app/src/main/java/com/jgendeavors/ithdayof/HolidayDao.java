3ab62a37422ea784d200aad93acbf9edd2d4b6ca
Jarad Gray <jarad.h.gray@gmail.com>
2020-01-03 15:08:10 -0600

    implement HolidayDao

--- app/src/main/java/com/jgendeavors/ithdayof/HolidayDao.java
+++ app/src/main/java/com/jgendeavors/ithdayof/HolidayDao.java
@@ -1,4 +1,34 @@
 package com.jgendeavors.ithdayof;
 
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+/**
+ * The DAO (Data Access Object) provides abstracted access to the Room database
+ * We define methods that correspond to database operations, and Room implements the methods
+ * under the hood.
+ *
+ * Rule of thumb: create one DAO per Room entity
+ */
+@Dao
 public interface HolidayDao {
+    @Insert
+    void insert(Holiday holiday);
+    @Update
+    void update(Holiday holiday);
+    @Delete
+    void delete(Holiday holiday);
+
+    @Query("SELECT * FROM holiday_table ORDER BY date DESC")
+    LiveData<List<Holiday>> getHolidaysByDateDescending();
+    @Query("SELECT * FROM holiday_table ORDER BY date_created DESC")
+    LiveData<List<Holiday>> getHolidaysByDateCreatedDescending();
+    @Query("SELECT * FROM holiday_table ORDER BY title DESC")
+    LiveData<List<Holiday>> getHolidaysByTitleDescending();
 }
