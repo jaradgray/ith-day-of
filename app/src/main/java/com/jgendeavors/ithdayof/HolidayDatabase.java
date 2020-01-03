224485f6139855d08351e4a4b3af735bcf144c54
Jarad Gray <jarad.h.gray@gmail.com>
2020-01-03 15:12:09 -0600

    implement HolidayDatabase

--- app/src/main/java/com/jgendeavors/ithdayof/HolidayDatabase.java
+++ app/src/main/java/com/jgendeavors/ithdayof/HolidayDatabase.java
@@ -1,4 +1,27 @@
 package com.jgendeavors.ithdayof;
 
-public class HolidayDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+/**
+ * Singleton pattern
+ */
+@Database(entities = {Holiday.class}, version = 1)
+public abstract class HolidayDatabase extends RoomDatabase {
+    private static HolidayDatabase instance;
+
+    public abstract HolidayDao holidayDao(); // Room implements this method under the hood with the databaseBuilder() method
+
+    public static synchronized HolidayDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    HolidayDatabase.class, "holiday_database")
+                    .fallbackToDestructiveMigration() // delete this database when version number is incremented
+                    .build();
+        }
+        return instance;
+    }
 }
